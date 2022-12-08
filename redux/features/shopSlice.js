756fae1f008ddfcfd58bbee0bc13c3118b41e0e6
Andrew Dobson <tandrewlopez@gmail.com>
2022-12-08 18:18:01 -0500

    created async thunk for fetching all vinyls from db

--- redux/features/shopSlice.js
+++ redux/features/shopSlice.js
@@ -1,10 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const shopSlice = createSlice({
   name: "shop",
   initialState: {
     allVinyls: [],
+    isLoading: false,
+    shopError: null,
   },
+  extraReducers: (builder) => {
+    builder.addCase(getVinyls.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getVinyls.rejected, (state, action) => {
+      state.isLoading = false;
+      state.allVinyls = [];
+      state.shopError = action.payload;
+    });
+    builder.addCase(getVinyls.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.allVinyls = action.payload.sort((a, b) => a.id - b.id);
+    });
+  },
+});
+
+export const getVinyls = createAsyncThunk("getVinyls", async (thunkAPI) => {
+  const baseURL = "http://localhost:7000/api/";
+  const response = await fetch(baseURL + "shop", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+  return response;
 });
 
 export default shopSlice.reducer;
--- src/pages/AllVinylsPage.jsx
+++ src/pages/AllVinylsPage.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getVinyls } from "../../redux/features/shopSlice";
+
 const AllVinylsPage = () => {
+  const dispatch = useDispatch();
+  const {
+    shopReducer: { allVinyls, isLoading, shopError },
+  } = useSelector((state) => state);
+
+  useEffect(() => {
+    if (!allVinyls.length) {
+      dispatch(getVinyls());
+    }
+  }, []);
+
+  if (shopError) {
+    return <div>Error</div>;
+  }
+
   return (
     <div className="flex-1 flex justify-center bg-shade-7">
-      DISPLAY ALL PRODUCTS
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>{`${allVinyls.length} Vinyls`}</div>
+      )}
     </div>
   );
 };
